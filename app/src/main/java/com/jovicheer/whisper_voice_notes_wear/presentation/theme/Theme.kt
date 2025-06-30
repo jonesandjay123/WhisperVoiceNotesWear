3d6a6b9c4944dab3f60cc789b21086252ebfc3c8
joneswang <jonescwang@gmail.com>
2025-06-30 16:54:58 -0400

    first commit

---
+++ app/src/main/java/com/jovicheer/whisper_voice_notes_wear/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.jovicheer.whisper_voice_notes_wear.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun WhisperVoiceNotesWearTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
