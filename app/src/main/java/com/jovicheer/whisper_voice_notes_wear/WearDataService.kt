babc79f802d140e9f13ddeb0713571998f4c2c01
joneswang <jonescwang@gmail.com>
2025-06-30 19:50:49 -0400

    try to fetch data from mobile

--- app/src/main/java/com/jovicheer/whisper_voice_notes_wear/WearDataService.kt
+++ app/src/main/java/com/jovicheer/whisper_voice_notes_wear/WearDataService.kt
@@ -4,8 +4,9 @@ import android.util.Log
 import com.google.android.gms.wearable.MessageEvent
 import com.google.android.gms.wearable.WearableListenerService
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.jovicheer.whisper_voice_notes_wear.data.NotesRepository
-import com.jovicheer.whisper_voice_notes_wear.data.SyncResponse
+import com.jovicheer.whisper_voice_notes_wear.data.TranscriptionRecord
 
 class WearDataService : WearableListenerService() {
     
@@ -27,21 +28,20 @@ class WearDataService : WearableListenerService() {
     private fun handleSyncResponse(data: ByteArray) {
         try {
             val json = String(data)
-            Log.d("WearDataService", "收到JSON: $json")
+            Log.d("WearDataService", "收到原始 JSON: $json")
             
-            val response = gson.fromJson(json, SyncResponse::class.java)
-            
-            if (response.success) {
-                Log.d("WearDataService", "收到 ${response.records.size} 筆記錄")
-                notesRepository.updateNotes(response.records)
-            } else {
-                Log.w("WearDataService", "同步失敗")
-            }
-            
-            notesRepository.setLoading(false)
+            // 手機端現在直接發送筆記列表的 JSON 字符串
+            // 我們使用 TypeToken 來正確解析這個列表
+            val listType = object : TypeToken<List<TranscriptionRecord>>() {}.type
+            val records: List<TranscriptionRecord> = gson.fromJson(json, listType)
+
+            Log.d("WearDataService", "成功解析 ${records.size} 筆記錄")
+            notesRepository.updateNotes(records)
             
         } catch (e: Exception) {
-            Log.e("WearDataService", "解析數據失敗", e)
+            Log.e("WearDataService", "解析筆記列表 JSON 失敗", e)
+        } finally {
+            // 無論成功或失敗，都結束載入狀態
             notesRepository.setLoading(false)
         }
     }
